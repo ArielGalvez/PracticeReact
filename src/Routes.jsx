20ea8e03c251a73533b29eeb1dd9fb606f77de1b
Ariel Galvez <Ariel.Galvez@fundacion-jala.org>
2019-04-09 19:52:00 -0400

    create Member in team

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch} from 'react-router-dom';
 import CreateTeam from './views/Team/CreateTeam';
+import EditTeam from './views/Team/EditTeam';
 import Navbar from './views/Navbar';
 function Routes() {
   return (
@@ -8,9 +9,8 @@ function Routes() {
       <BrowserRouter>
         <Switch>
           <Route exact path="/teams/create" component={CreateTeam} />
-          <Route path="/" component={Navbar} >
-            {/* <Navbar></Navbar> */}
-          </Route>
+          <Route exact path="/teams/edit" component={EditTeam} />
+          <Route path="/" component={Navbar} />
         </Switch>
       </BrowserRouter>
     </React.Fragment>
